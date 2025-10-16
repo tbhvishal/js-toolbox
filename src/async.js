4483d3a133ff905a6ed4ab4e1aa8e82c64e9a07e
Vishal Sharma <thatrealvishal@gmail.com>
2025-10-16 14:21:41 +0530

    clean up

--- src/async.js
+++ src/async.js
@@ -90,6 +90,6 @@ export  function  throttle(fn,  limitMs  =  300)  {
 //  minor  update
 
 
-// minor update
-// minor update
-// minor update
+//  minor  update
+//  minor  update
+//  minor  update
