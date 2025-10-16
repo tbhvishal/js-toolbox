dba06aa2dde3f918844bebed7fb810a007334e81
Vishal Sharma <thatrealvishal@gmail.com>
2025-10-16 14:23:25 +0530

    update docs

--- src/async.js
+++ src/async.js
@@ -111,5 +111,5 @@ export  function  throttle(fn,  limitMs  =  300)  {
 //  minor  update
 //  minor  update
 //  minor  update
-// minor update
-// minor update
+//  minor  update
+//  minor  update
