fd744a9fa268e98a9460e9ec3d3fdf0b3dc9263e
Vishal Sharma <thatrealvishal@gmail.com>
2025-10-16 14:18:41 +0530

    refactor

--- tests/strings.test.js
+++ tests/strings.test.js
@@ -1,15 +1,15 @@
-import assert from 'node:assert/strict';
-import * as strings from '../src/strings.js';
+import  assert  from  'node:assert/strict';
+import  *  as  strings  from  '../src/strings.js';
 
-assert.equal(strings.capitalize('hey'), 'Hey');
-assert.equal(strings.kebabCase('HelloWorld'), 'hello-world');
-assert.equal(strings.slugify('Café Déjà Vu'), 'cafe-deja-vu');
-assert.equal(strings.truncate('hello', 3), 'hel…');
-assert.equal(strings.pad('a', 3, '.'), 'a..');
-assert.equal(strings.reverse('abc'), 'cba');
-assert.equal(strings.camelCase('hello-world'), 'helloWorld');
-assert.equal(strings.snakeCase('HelloWorld'), 'hello_world');
-assert.equal(strings.repeat('x', 3), 'xxx');
-assert.equal(strings.escapeHtml('<div>'), '&lt;div&gt;');
+assert.equal(strings.capitalize('hey'),  'Hey');
+assert.equal(strings.kebabCase('HelloWorld'),  'hello-world');
+assert.equal(strings.slugify('Café  Déjà  Vu'),  'cafe-deja-vu');
+assert.equal(strings.truncate('hello',  3),  'hel…');
+assert.equal(strings.pad('a',  3,  '.'),  'a..');
+assert.equal(strings.reverse('abc'),  'cba');
+assert.equal(strings.camelCase('hello-world'),  'helloWorld');
+assert.equal(strings.snakeCase('HelloWorld'),  'hello_world');
+assert.equal(strings.repeat('x',  3),  'xxx');
+assert.equal(strings.escapeHtml('<div>'),  '&lt;div&gt;');
 
-console.log('strings tests ok');
+console.log('strings  tests  ok');
