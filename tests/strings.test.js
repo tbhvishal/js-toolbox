eadd59eabf27c91c2def0bb46990d72cb93c4b38
Vishal Sharma <thatrealvishal@gmail.com>
2025-10-16 13:47:35 +0530

    Update string tests

--- tests/strings.test.js
+++ tests/strings.test.js
@@ -6,5 +6,10 @@ assert.equal(strings.kebabCase('HelloWorld'), 'hello-world');
 assert.equal(strings.slugify('Café Déjà Vu'), 'cafe-deja-vu');
 assert.equal(strings.truncate('hello', 3), 'hel…');
 assert.equal(strings.pad('a', 3, '.'), 'a..');
+assert.equal(strings.reverse('abc'), 'cba');
+assert.equal(strings.camelCase('hello-world'), 'helloWorld');
+assert.equal(strings.snakeCase('HelloWorld'), 'hello_world');
+assert.equal(strings.repeat('x', 3), 'xxx');
+assert.equal(strings.escapeHtml('<div>'), '&lt;div&gt;');
 
 console.log('strings tests ok');
